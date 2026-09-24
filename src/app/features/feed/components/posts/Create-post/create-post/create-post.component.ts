0e2b3746d7b358e88d4fc813db0180f7077407d1
Youssef samir <usef.route@gmail.com>
2026-09-24 18:17:24 +0300

    seperate posts page

---
+++ src/app/features/feed/components/posts/Create-post/create-post/create-post.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-post',
+  imports: [],
+  templateUrl: './create-post.component.html',
+  styleUrl: './create-post.component.css',
+})
+export class CreatePostComponent {}
--- src/app/features/feed/components/posts/posts.component.ts
+++ src/app/features/feed/components/posts/posts.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { PostsService } from '../../../../core/auth/services/Posts/posts.service';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CreatePostComponent } from './Create-post/create-post/create-post.component';
 
 @Component({
   selector: 'app-posts',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive , CreatePostComponent],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css',
 })
